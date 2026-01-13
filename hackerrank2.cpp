6db0221a65d1f83d326f3d52abb85fd0a6a1ba19
Rohan Kumar Sharma <your_github_email@gmail.com>
2026-01-13 16:48:58 +0530

    3rd question

---
+++ hackerrank2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number" << endl;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << a * 2 << endl;
+    cout << b << endl;
+}
